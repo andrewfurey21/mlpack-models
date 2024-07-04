d8f90e6c6b46d1df12c68ec0ace325b176461d36
Andrew Furey <andrewfurey2003@gmail.com>
2024-07-04 13:58:43 +0100

    added mask and anchors, updated maxpool block

--- models/yolov3_tiny/yolov3_layer.hpp
+++ models/yolov3_tiny/yolov3_layer.hpp
@@ -1,12 +1,98 @@
-
+/**
+ * @author Andrew Furey
+ *
+ * Definition of the YOLO layer for object detection
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
 #ifndef YOLO_LAYER_HPP
 #define YOLO_LAYER_HPP
 
 #include <mlpack/prereqs.hpp>
-#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann.hpp>
 
 namespace mlpack {
-}
 
+/*
+ * Implementation of the yolo layer for object detection. 
+ *
+ * @tparam MatType Matrix representation to accept as input and use for
+ *    computation.
+ */
+template<typename MatType>
+class Yolo : public Layer<MatType>
+{
+ public:
+  /**
+   * Create the Yolo object.  The output size of the layer will be the same
+   * as the input size.
+   */
+  Yolo();
+
+  //! Clone the Yolo object. This handles polymorphism correctly.
+  Yolo* Clone() const { return new Yolo(*this); }
+
+  // Virtual destructor.
+  virtual ~Yolo() { }
+
+  //! Copy the given Yolo layer.
+  Yolo(const Yolo& other);
+  //! Take ownership of the given Yolo layer.
+  Yolo(Yolo&& other);
+  //! Copy the given Yolo layer.
+  Yolo& operator=(const Yolo& other);
+  //! Take ownership of the given Yolo layer.
+  Yolo& operator=(Yolo&& other);
+
+  /**
+   * Forward pass
+   */
+  void Forward(const MatType& input, MatType& output);
+
+  /**
+   * Backward pass
+   */
+  void Backward(const MatType& /* input */,
+                const MatType& /* output */,
+                const MatType& gy,
+                MatType& g);
+
+  /**
+   * Calculate the gradient using the output and the input activation.
+   *
+   * @param * (input) The propagated input.
+   * @param error The calculated error.
+   * @param gradient The calculated gradient.
+   */
+  void Gradient(const MatType& /* input */,
+                const MatType& error,
+                MatType& gradient);
+
+  //! Return the weights of the network.
+  const MatType& Parameters() const { return weights; }
+  //! Modify the weights of the network.
+  MatType& Parameters() { return weights; }
+
+  //! Compute the output dimensions of the layer, based on the internal values
+  //! of `InputDimensions()`.
+  void ComputeOutputDimensions();
+
+  //! Set the weights of the layer to use the given memory.
+  void SetWeights(const MatType& weightsIn);
+
+  /**
+   * Serialize the layer.
+   */
+  template<typename Archive>
+  void serialize(Archive& ar, const uint32_t /* version */);
+
+ private:
+  //! Locally-stored weight object.
+  MatType weights;
+}; // Yolo class
+} // namespace mlpack
 
 #endif
--- models/yolov3_tiny/yolov3_tiny.hpp
+++ models/yolov3_tiny/yolov3_tiny.hpp
@@ -1,9 +1,7 @@
 #ifndef MODELS_MODELS_YOLOV3_TINY_YOLOV3_TINY_HPP
 #define MODELS_MODELS_YOLOV3_TINY_YOLOV3_TINY_HPP
 
-#include <mlpack/methods/ann/layer/adaptive_max_pooling.hpp>
-#include <mlpack/methods/ann/layer/add.hpp>
-#include <mlpack/methods/ann/layer/batch_norm.hpp>
+#include <mlpack/methods/ann/layer/convolution.hpp>
 #include <mlpack/methods/ann/layer/max_pooling.hpp>
 #include <mlpack/methods/ann/layer/multi_layer.hpp>
 
@@ -16,9 +14,14 @@ namespace models {
 template<typename MatType>
 class YoloV3Tiny {
 public:
+  /**
+  * 
+  */
   YoloV3Tiny(const size_t inputWidth,
 	     const size_t inputHeight,
 	     const size_t inputChannels,
+	     const std::vector<size_t> mask,
+	     const std::vector<double> anchors,
 	     const size_t numClasses = 80,
 	     const float ignoreThresh = 0.7
 	     ) :
@@ -27,30 +30,47 @@ public:
 	inputChannels(inputChannels),
 	numClasses(numClasses),
 	ignoreThresh(ignoreThresh)
+
 	{}
 
   ~YoloV3Tiny() {}
 
   void LoadModel(const std::string &filePath);
   void SaveModel(const std::string &filePath);
+  void Detect(const std::string& filePath);
 
 private:
-  MultiLayer<MatType>* MaxPoolBlock(const size_t size) {
+  Layer<MatType>* MaxPoolBlock(const size_t size) {
     return new MaxPooling(inputWidth / (double)size, inputHeight / (double)size);
   }
 
-  void Darknet19() {}
+  Layer<MatType>* ConvolutionalBlock(const size_t channels, 
+				     const size_t kernelSize, 
+				     const size_t filters, 
+				     const size_t stride) 
+  {
+    return new Convolution(channels, 
+			   kernelSize, 
+			   kernelSize, 
+			   stride, 
+			   stride);
+  }
+
+  void Darknet19();
+  void GetBox();
+  void NonMaxSuppression();
 
   size_t inputWidth;
   size_t inputHeight;
   size_t inputChannels;
   size_t numClasses;
   float ignoreThresh;
+
+  std::vector<size_t> mask;
+  std::vector<double> anchors;
 };
 
 } // namespace models
 } // namespace mlpack
-
-#include "yolov3_tiny_impl.hpp"
 
 #endif

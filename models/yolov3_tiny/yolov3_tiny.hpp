9e4d14c8e171908c644d9f4605eae4ce3d6d326b
Andrew Furey <andrewfurey2003@gmail.com>
2024-07-07 01:49:39 +0100

    fixed pooling layer

--- models/yolov3_tiny/yolov3_tiny.hpp
+++ models/yolov3_tiny/yolov3_tiny.hpp
@@ -1,9 +1,13 @@
 #ifndef MODELS_MODELS_YOLOV3_TINY_YOLOV3_TINY_HPP
 #define MODELS_MODELS_YOLOV3_TINY_YOLOV3_TINY_HPP
 
+#include <mlpack/methods/ann/init_rules/random_init.hpp>
 #include <mlpack/methods/ann/layer/convolution.hpp>
 #include <mlpack/methods/ann/layer/max_pooling.hpp>
 #include <mlpack/methods/ann/layer/multi_layer.hpp>
+
+#include "yolov3_tiny_loss.hpp"
+#include "yolov3_tiny_layer.hpp"
 
 #define MLPACK_ENABLE_ANN_SERIALIZATION
 #include <mlpack.hpp>
@@ -14,11 +18,11 @@ namespace models {
 template<typename MatType>
 class YoloV3Tiny {
 public:
-  YoloV3Tiny(const size_t inputWidth,
-	     const size_t inputHeight,
-	     const size_t inputChannels,
-	     const std::vector<size_t> mask,
+  YoloV3Tiny(const std::vector<size_t> mask,
 	     const std::vector<double> anchors,
+	     const size_t inputWidth = 416,
+	     const size_t inputHeight = 416,
+	     const size_t inputChannels = 3,
 	     const size_t numClasses = 80
 	     ) :
 	inputWidth(inputWidth),
@@ -29,15 +33,12 @@ public:
 
   ~YoloV3Tiny() {}
 
-  void LoadModel(const std::string &filePath) {}
-  void SaveModel(const std::string &filePath) {}
-
 private:
-  Layer<MatType>* MaxPoolBlock(const size_t size) {
-    return new MaxPooling(inputWidth / (double)size, inputHeight / (double)size);
+  void AddPoolingBlock(const size_t kernel, const size_t stride) {
+    model.Add(MaxPooling(kernel, kernel, stride, stride));
   }
 
-  FFN<> model;
+  FFN<YoloV3TinyLoss<MatType>, RandomInitialization> model;
 
   size_t inputWidth;
   size_t inputHeight;
